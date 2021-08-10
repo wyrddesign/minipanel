3bafea90f8824231a5e01e4b5e0ca053e194006a
Wyrd Design <charlie@wyrd.design>
2021-08-10 11:25:34 -0700

    Moved browser-specific serial connection code to minipanel.js now

--- src/chrome_extension/minipanel.js
+++ src/chrome_extension/minipanel.js
@@ -1,10 +1,88 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+class MiniPanelSerial {
+    constructor(serialPort) {
+        this.serialPort = serialPort;
+    }
+    
+    async open() {
+        await this.serialPort.open({baudRate: 9600});
+    }
+
+    async send(message) {
+        const writer = this.serialPort.writable.getWriter();
+        await writer.write(dumpMessage(message));
+        writer.releaseLock();
+    }
+
+    async *receive() {
+        const bytes = [];
+        const messageParser = parseMessages();
+        messageParser.next();
+        let reader;
+    
+        try {
+            while(this.serialPort.readable) {
+                reader = this.serialPort.readable.getReader();   
+                const { value, done } = await reader.read();
+    
+                // Release the lock before yielding a value in case the iterator is never used again
+                reader.releaseLock();
+                bytes.push(...value);
+                while(bytes.length) {
+                    const byte = bytes.shift();
+                    const {value} = messageParser.next(byte);
+                    if (value) {
+                        yield value;
+                    }
+                }
+                if (done) {
+                    break;
+                }
+            }
+        } finally {
+            if (reader) {
+                reader.releaseLock();
+            }
+        }
+    }
+
+    async close() {
+        await this.serialPort.close();
+    }
+
+    getInfo() {
+        return this.serialPort.getInfo();
+    }
+}
+
 class MiniPanel {    
     static deviceFilters = [
         {usbProductId: 29987, usbVendorId: 6790}
     ];
 
+    static async probe(serialPort) {
+        const serial = new MiniPanelSerial(serialPort);
+        await serial.open();
+        
+        // Wait for the Arduino to reset
+        await sleep(3000);
+        await serial.send(new ProbeMessage());
+        const {value} = await serial.receive().next();
+    
+        // Return true if a MiniPanel was found
+        if (value instanceof ProbeMessage) {
+            return true;
+        } else {
+            serial.close();
+            return false;
+        }
+    }
+
     static async fromSerialPort(serialPort) {
-        if (serialPort && await serialPort.probe()) {
+        if (serialPort && await this.probe(serialPort)) {
             return new MiniPanel(serialPort);
         }
     }
@@ -30,33 +108,35 @@ class MiniPanel {
     }
 
     constructor(serialPort) {
-        this.serialPort = serialPort;
+        this.serial = new MiniPanelSerial(serialPort);
     }
 
     async setModeMultiKey() {
-        await this.serialPort.sendMessage(new Message(MSG_TYPE_MODE_MULTI_KEY, 0x0));
+        await this.serial.send(new ModeMessage(MSG_MODE_MULTI_KEY));
+    }
+
+    async setModeSingleKey() {
+        await this.serial.send(new ModeMessage(MSG_MODE_SINGLE_KEY));
     }
 
     async send(message) {
-        await this.serialPort.sendMessage(message);
+        await this.serial.send(message);
     }
 
     async listenForever(callback) {
-        for await (const message of this.serialPort.receiveMessages()) {
+        for await (const message of this.serial.receive()) {
             if (message) {
                 // Let the callback run asynchronously so that new messages aren't blocked.
                 callback(message).then((message) => {
                     if (message) {
-                        this.serialPort.sendMessage(message);
+                        this.serial.send(message);
                     }
                 });
             }
         }
     }
 
-    
-
     async close() {
-        await this.serialPort.close();
+        await this.serial.close();
     }
 }
